45a9b08a3f7275a6c298326f9b033f6b2b08b98f
Shresta Vagdevi <shrestavagdevi@gmail.com>
2025-10-13 05:45:31 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        suppressHydrationWarning={true}
         className={cn(
           'min-h-screen bg-background font-body antialiased',
           inter.variable,
